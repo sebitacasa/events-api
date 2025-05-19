f7f34d201b11b8b0348ebd9df659e5fd09c98cda
sebitacasa <seba.-14@hotmail.com>
2025-05-19 19:41:23 +1000

    db updated

--- src/db.js
+++ src/db.js
@@ -4,13 +4,21 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/eventos`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
+const { DATABASE_URL } = process.env;
+
+
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/eventos`,
+//   {
+//     logging: false, // set to console.log to see the raw SQL queries
+//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   }
+// );
+
+const sequelize = new Sequelize(DATABASE_URL, {
+  logging: false,
+  native: false,
+});
 
 const basename = path.basename(__filename);
 
