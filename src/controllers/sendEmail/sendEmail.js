c44b448ad61d8d8e9d674bea7a68d686463ec9e1
sebitacasa <seba.-14@hotmail.com>
2026-01-25 12:11:16 -0300

    engines node actualizado

--- src/controllers/sendEmail/sendEmail.js
+++ src/controllers/sendEmail/sendEmail.js
@@ -5,16 +5,19 @@ const { MAILUSER, MAILPASS } = process.env;
 
 async function sendEmail(userEmail, content) {
 
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "blackwilson1495@gmail.com", // generated ethereal user
-      pass: "jagkviygqxxhlzth", // generated ethereal password
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
+let transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com", // <--- ¡Asegúrate de que esta línea esté escrita así!
+  port: 587,
+  secure: false, // false para puerto 587
+  auth: {
+    user: process.env.MAILUSER,
+    pass: process.env.MAILPASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  family: 4, // <--- ¡ESTA ES LA CLAVE! Fuerza a usar IPv4 en lugar de IPv6 (::1)
+});
 
   let info = await transporter.sendMail({
     from: "blackwilson1495@gmail.com", // sender address
